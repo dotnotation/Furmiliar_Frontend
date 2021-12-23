b5c590c6bc82320609a4f0e72c3a30b9b3d6cd03
dotnotation <dotinmotionproductions@gmail.com>
2021-12-23 13:33:40 -0500

    added slight styling and adding button to toy box card

--- src/components/toyBox.js
+++ src/components/toyBox.js
@@ -7,14 +7,21 @@ class ToyBox {
     }
 
     addToDom(){
-        const toyBoxCard = document.getElementById("toy-box")
+        const toyBoxDiv = document.getElementById("toy-box")
+        const toyBoxCard = document.createElement("div")
+        toyBoxCard.className = "toy-box-card"
         const photoBox = document.createElement("img")
         photoBox.src = this.photo
+        photoBox.className = "toy-box-photo"
         const toyBoxName = document.createElement("h2")
         toyBoxName.innerText = this.name
+        const openToyBox = document.createElement("button")
+        openToyBox.innerText = "Open Toy Box" 
 
+        toyBoxDiv.appendChild(toyBoxCard)
         toyBoxCard.appendChild(photoBox)
         toyBoxCard.appendChild(toyBoxName)
+        toyBoxCard.appendChild(openToyBox)
 
         return toyBoxCard
     }
