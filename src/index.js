1d1d8cf9a6bf95605650d248504718933be6e983
dotnotation <dotinmotionproductions@gmail.com>
2022-01-18 16:55:36 -0500

    added live coding section from assessment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const toyBoxAdapter = new ToyBoxAdapter(baseURL)
 const toyAdapter = new ToyAdapter(baseURL)
 let editMode = false
 let toyEditMode = false
-let currentToys = false
+// let currentToys = false
 const toyBoxForm = new ToyBoxForm
 const toyForm = new ToyForm
 
@@ -13,3 +13,30 @@ document.addEventListener("DOMContentLoaded", () => {
     toyBoxForm.listenEvents()
     toyForm.listenToys()
 })
+
+function userComments(){
+    const submitBtn = document.getElementById("user-comments-submit")
+    // console.log(submitBtn.parentElement.children)
+    // console.log(userInput)
+    submitBtn.addEventListener("click", handleComments)
+    // event listener on submit
+    // we need to grab values from form
+    // clear the form
+}
+
+userComments()
+
+function handleComments(e){
+    // debugger
+    e.preventDefault()
+    console.log("reached handleComments")
+    console.log(e.target.parentElement.children)
+    const userInput = e.target.parentElement.children[0].value
+    const commentDiv = document.getElementById("user-comments-div")
+    // debugger
+    commentDiv.append(userInput)
+    console.log(userInput)
+    e.target.parentElement.children[0].value = ""
+    commentDiv.innerHTML += `<br>`
+
+}
